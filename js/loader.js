ec35cf0f3e2971bb375424558c07e2a57e538669
benzin <celesnicamusic08@gmail.com>
2026-07-10 18:05:50 +0800

    Change '20BS' to '2ØBS' in fakeTextMap

--- js/loader.js
+++ js/loader.js
@@ -43,7 +43,7 @@ const Loader = (() => {
             const original = element.textContent.trim();
 
             const fakeTextMap = {
-                "2088": "20BS",
+                "2088": "2ØBS",
                 "RECORDS": "RΞƇØΓÐ$"
             };
 
